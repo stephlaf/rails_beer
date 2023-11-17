44110d9c1c504c61457b0b5c903f4e510a7b06c0
Stephane Lafontaine <stephane@medqigong.ca>
2023-11-17 18:38:00 -0500

    Fixed rubocop offenses

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
-class Api::V1::BreweriesController < Api::V1::BaseController
-  def all
-    @breweries = Brewery.all.order_by_name
-  end
+module Api
+  module V1
+    # BreweriesController
+    class BreweriesController < Api::V1::BaseController
+      def all
+        @breweries = Brewery.all.order_by_name
+      end
 
-  def search
-    @breweries = Brewery.global_search(params[:query]).order_by_name
-  end
+      def search
+        @breweries = Brewery.global_search(params[:query]).order_by_name
+      end
 
-  def show
-    @brewerie = Brewery.find(params[:id])
+      def show
+        @brewerie = Brewery.find(params[:id])
+      end
+    end
   end
 end
--- lib/tasks/load_beers.rake
+++ lib/tasks/load_beers.rake
@@ -1,4 +1,6 @@
-desc "Print something five times."
+# frozen_string_literal: true
+
+desc 'Print something five times.'
 task :test_rake do
   p 'Testing rake'
 end
